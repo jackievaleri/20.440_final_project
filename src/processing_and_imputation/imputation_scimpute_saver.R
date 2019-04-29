5967f392956c499e7073c67c4fd0cefd1d4329c8
Jackie Valeri <valerij@mit.edu>
2019-04-29 18:37:27 -0400

    imputation and repopulating folders

---
+++ src/processing_and_imputation/imputation_scimpute_saver.R
@@ -0,0 +1,48 @@
+## Part 1: ScImpute
+#install_github("Vivianstats/scImpute") # need to do this if not already installed
+library(scImpute)
+
+# just use the first patient as this is representative
+# note: if running in R studio, may need to change the path to be local to your machine
+data_path = '../../data/filtered_data/HIV1_Bld.csv'
+num_clusters = 20
+out_path = "../../data/imputation_intermediate/different_imputations/scimpute_intermediates/"
+
+# Run scimpute
+scimpute(# full path to raw count matrix
+  count_path = data_path, 
+  infile = "csv",           # format of input file
+  outfile = "csv",          # format of output file
+  out_dir = out_path,           # full path to output directory
+  labeled = FALSE,          # cell type labels not available
+  drop_thre = 0.5,          # threshold set on dropout probability
+  Kcluster = num_clusters,             # 23 cell subpopulations?
+  ncores = 10)              # number of cores used in parallel computation
+
+## Part 2: Saver
+#install.packages("SAVER") # need to do this if not already installed
+library(SAVER)
+#install.packages("doSNOW") # need to do this if not already installed
+library(doSNOW)
+#install.packages("parallel") # need to do this if not already installed
+library(parallel)
+
+# detect cores with parallel() package
+nCores <- detectCores(logical = FALSE)
+cat(nCores, "cores detected.")
+
+# parallelize the computation
+library(doParallel)
+cl <- makeCluster(6) # fill in with your number of cores detected
+registerDoParallel(cl)
+
+# read in the filtered data
+data = read.csv(data_path)
+X <- data[, 2:ncol(data) ]
+
+# perform the SAVER imputation
+X.saver <- saver(X, ncores = 6)
+str(X.saver$estimate)
+out_path = "../../data/imputation_intermediate/different_imputations/"
+out_name = out_path + 'saver_hiv1_bld.csv'
+write.csv(X.saver$estimate, out_name)
