44c819481dd3e47536eff76f8a5a4415c2e10f67
Jackie Valeri <valerij@mit.edu>
2019-04-30 16:21:44 -0400

    clean up figure making barplot

--- src/figure_making/supp_figure_2CD_barplots.R
+++ src/figure_making/supp_figure_2CD_barplots.R
@@ -4,12 +4,11 @@ library(RColorBrewer)
 library(ggpubr)
 
 # Part 1: Table with Non-Imputed Counts
-#original_table = read.table('../../data/seurat_intermediates/original_table')
-original_table = read.table('original_table')
+original_table = read.table('../../data/seurat_intermediates/original_table')
 original_frame = as.matrix(original_table)
 original_frame = prop.table(original_frame, 2)
 cols = RColorBrewer::brewer.pal(name = "Paired", n=12)
-jpeg("fig_2_nonimputed_barplot.jpg", width = 1200, height = 600, pointsize = 15, res = 75)
+jpeg("../../output/figures/supp_fig_2C_nonimputed_barplot.jpg", width = 1200, height = 600, pointsize = 15, res = 75)
 original_barplot = barplot(original_frame, col = cols, xlim =c(0,11), xaxt = "n", ylab = 'Fraction Cells', cex.axis=1.1, cex.lab=1.1)
 original_barplot = legend('right', legend = rownames(original_frame), fill=cols, xpd=TRUE, horiz=FALSE, cex = 1, 
                           x.intersp = 0.15, x = 8.5, y = 1)
@@ -17,11 +16,10 @@ original_barplot = axis(side = 1, labels = c('HIV+1,Bld', 'HIV+1,CSF', 'HIV+2,Bl
 dev.off()
 
 # Part 2: Do MAGIC on the integrated data object
-#imputed_table = read.table('../../data/seurat_intermediates/imputed_table')
-imputed_table = read.table('imputed_table')
+imputed_table = read.table('../../data/seurat_intermediates/imputed_table')
 imputed_frame = as.matrix(imputed_table)
 imputed_frame = prop.table(imputed_frame, 2)
-jpeg("fig_2_imputed_barplot.jpg", width = 1200, height = 600, pointsize = 15, res = 75)
+jpeg("../../output/figures/supp_fig_2D_imputed_barplot.jpg", width = 1200, height = 600, pointsize = 15, res = 75)
 imputed_barplot = barplot(imputed_frame, col = cols, xlim =c(0,11), xaxt = "n", ylab = 'Fraction Cells', cex.axis=1.1, cex.lab=1.1)
 imputed_barplot = legend('right', legend = rownames(original_frame), fill=cols, xpd=TRUE, horiz=FALSE, cex = 1, 
                          x.intersp = 0.15, x = 8.5, y = 1)
