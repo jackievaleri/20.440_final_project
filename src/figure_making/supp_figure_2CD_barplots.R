0ce2dda0a75cbc3466a0296bef6964ebaf3a9c1b
Jackie Valeri <valerij@mit.edu>
2019-04-30 17:00:50 -0400

    updating paths for the python notebooks

--- src/figure_making/supp_figure_2CD_barplots.R
+++ src/figure_making/supp_figure_2CD_barplots.R
@@ -3,8 +3,10 @@ library(ggplot2)
 library(RColorBrewer)
 library(ggpubr)
 
+# NOTE: May need to setwd() if using RStudio, as this is different for each individual machine.
+
 # Part 1: Table with Non-Imputed Counts
-original_table = read.table('../../data/seurat_intermediates/original_table')
+original_table = read.table('../../data/seurat_tables/original_table')
 original_frame = as.matrix(original_table)
 original_frame = prop.table(original_frame, 2)
 cols = RColorBrewer::brewer.pal(name = "Paired", n=12)
@@ -15,8 +17,8 @@ original_barplot = legend('right', legend = rownames(original_frame), fill=cols,
 original_barplot = axis(side = 1, labels = c('HIV+1,Bld', 'HIV+1,CSF', 'HIV+2,Bld', 'HIV+2,CSF', 'HIV+3,CSF', 'HIV-1,CSF', 'HIV-2,CSF'), at = c(0.8, 2, 3.2, 4.4, 5.6, 6.8, 8))
 dev.off()
 
-# Part 2: Do MAGIC on the integrated data object
-imputed_table = read.table('../../data/seurat_intermediates/imputed_table')
+# Part 2: Table with the Imputed Counts
+imputed_table = read.table('../../data/seurat_tables/imputed_table')
 imputed_frame = as.matrix(imputed_table)
 imputed_frame = prop.table(imputed_frame, 2)
 jpeg("../../output/figures/supp_fig_2D_imputed_barplot.jpg", width = 1200, height = 600, pointsize = 15, res = 75)
